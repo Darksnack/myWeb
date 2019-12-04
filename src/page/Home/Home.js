8f471db1afbdca933d20d822467fef9069dd0a33
Darksnack <yqksnake@126.com>
2019-12-04 17:30:42 +0800

    首页更新

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -2,14 +2,13 @@
  * @Description: 
  * @Version: 1.0
  * @Date: 2019-12-02 09:12:00
- * @LastEditTime: 2019-12-03 17:26:25
+ * @LastEditTime: 2019-12-04 17:03:40
  */
 import React from 'react';
 import 'antd/dist/antd.css'
 import '../../resource/Icon/iconfont.css'
 import './Home.css'
-import { Icon } from 'antd';
-import { MenuItemGroup } from 'rc-menu';
+import { Icon, } from 'antd';
 export default class Home extends React.Component{
     constructor(props){
         super(props)
@@ -24,18 +23,6 @@ export default class Home extends React.Component{
     }
     handleClik = e => {
         console.log(e)
-    }
-    creatCard = () => {
-        let cardElement = []
-        this.state.homeCard.map((item,i)=>{
-            cardElement.push(
-                <div className="cardStyle">
-                    <a href={item.ADDRESS}>
-                        <span class={item.ICONNAME} style={{ color: item.COLOR }}>{item.ICONALT}</span>
-                    </a>
-                </div>
-            )
-        })
     }
     render(){
         return (
@@ -56,20 +43,46 @@ export default class Home extends React.Component{
                     </div>
                     <div className = 'innerContent'>
                         <div className='header'>
-                            <h3>我是青铜
-                            <h5>
-                                一名
-                                <span className='scrollText'>web前端开发</span>
-                                </h5>
-                            </h3>
+                            <span className='tittleOne'>我是青铜</span><span className='tittleTwo'>一名web前端开发</span>  
                         </div>
-                        <div className = 'content'>
-                            {this.creatCard}
-                        </div>
+                        <CardContent />
                     </div>
                 </div>
             </div>
 
+        )
+    }
+}
+class CardContent extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            homeCard:[
+                {"ICONNAME":"iconfont iconid-card","ICONALT":"关于我","COLOR":"#fa709a","ADDRESS":""},
+                {"ICONNAME":"iconfont iconjianlishaixuan","ICONALT":"我的简历","COLOR":"#f6d365","ADDRESS":""},
+                {"ICONNAME":"iconfont iconprojector-fill","ICONALT":"我的项目","COLOR":"#43e97b","ADDRESS":""},
+                {"ICONNAME":"iconfont iconmail","ICONALT":"联系我","COLOR":"#6991c7","ADDRESS":""},
+            ]
+        }
+    }
+    creatCard() {
+        let cardElement = []
+        this.state.homeCard.map((item) => {
+            cardElement.push(
+                <div className="cardStyle">
+                    <a href={item.ADDRESS}>
+                        <span class={item.ICONNAME} style={{ color: item.COLOR }}>{item.ICONALT}</span>
+                    </a>
+                </div>
+            )
+        })
+        return (
+            <div className='container'>{cardElement}</div>
+        )
+    }
+    render(){
+        return(
+            <div className='content'>{this.creatCard()}</div>
         )
     }
 }
